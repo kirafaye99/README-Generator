0a47d120a77147b6461b39cbeb28db1eee903267
kirafaye99 <kirademarco99@gmail.com>
2021-08-09 13:50:28 -0700

    almost finished

--- index.js
+++ index.js
@@ -1,20 +1,153 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const util = require('util');
+
+
 
 // TODO: Create an array of questions for user input
-const questions = [
-    inquirer
-        .prompt([
-            
-        ])
-];
+const questions = 
+            [
+            {
+                type: 'input',
+                message: 'What is your project called?',
+                name: 'title',
+            },
+            {
+                type: 'input',
+                message: 'Provide a short description of your project.',
+                name: 'description',
+            },
+            {
+                type: 'input',
+                message: 'What problem does your project solve?',
+                name: 'solution',
+            },
+            {
+                type: 'input',
+                message: 'What motivated you to create this project?',
+                name: 'motivation',
+            },
+            {
+                type: 'input',
+                message: 'What did you learn/What are some problems you faced while creating your project?',
+                name: 'learning',
+            },
+            {
+                type: 'input',
+                message: 'Is there anything that needs to be installed and, if so, how do you install?',
+                name: 'installation',
+            },
+            {
+                type: 'input',
+                message: 'Please provide some instructions for the use of your project.',
+                name: 'usage',
+            },
+            {
+                type: 'input',
+                message: 'Who were your collaborators/tutorials you followed?',
+                name: 'credit',
+            },
+            {
+                type: 'input',
+                message: 'Did you write any tests for your application? If so, how do you run them?',
+                name: 'tests',
+            },
+            {
+                type: 'list',
+                message: 'Which license do you want to use?',
+                choices:[
+                    "Apache",
+                    "Academic",
+                    "GNU",
+                    "ISC",
+                    "MIT",
+                    "Mozilla",
+                    "Open"
+                ],
+                name: 'license',
+            },
+            {
+                type: 'input',
+                message: 'What is your github username?',
+                name: 'github',
+            },
+            {
+                type: 'input',
+                message: 'What is your email address?',
+                name: 'email',
+            },
+            {
+                type: 'input',
+                message: 'How can people contact you?',
+                name: 'contact',
+            },
+            ];
+
+function generateMarkdown(response) {
+    let responses =
+    `# ${response.title}
+![License](https://img.shields.io/badge/license-${response.license}-brightgreen)
+## Description
+${response.description}
+- ${response.solution}
+- ${response.motivation}
+- ${response.learning}
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credit](#credit)
+- [License](#license)
+- [Tests](#tests)
+- [Questions](#questions)
+## Installation
+${response.installation}
+## Usage
+${response.usage}
+## Credit
+${response.credit}
+## License
+${response.license} License
+## Tests
+${response.tests}
+## Questions
+#### [My Github](https://github.com/${response.github})
+#### Email: 
+- ${response.email}
+#### How to reach out to me:
+- ${response.contact}
+`;
+return responses;
+}
+
+module.exports = generateMarkdown;
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log('Success!! 😊')
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init () {
+    try {
 
-// Function call to initialize app
+        const response = await inquirer.prompt(questions);
+        // console.log(response);
+
+        const markdown = generateMarkdown(response);
+        // console.log(markdown);
+
+        writeToFile('README.md', markdown);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// // Function call to initialize app
 init();
